6c481e31feb1a3cc0584196097eb2499c94d15c0
Kevin Yank <thatguy@kevinyank.com>
2023-06-05 23:21:30 +1000

    Port site to Eleventy

---
+++ index.md
@@ -0,0 +1,11 @@
+---
+title: All of LAMY
+permalink: "/"
+---
+
+This site aims to document a complete history of LAMY fountain pens.
+
+_Actual content coming soon!_
+
+Feel free to contact [Kevin Yank](https://kevinyank.com)
+if you have any questions.
---
+++ styles/tailwind.config.js
@@ -0,0 +1,33 @@
+const colors = require("tailwindcss/colors");
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./_includes/**/*.{html,liquid}",
+    "./_layouts/**/*.{html,liquid}",
+    "./*.{html,liquid}",
+  ],
+  theme: {
+    extend: {
+      borderColor: {
+        DEFAULT: "#ADB8AB",
+      },
+      colors: {
+        // {100: var(--theme-100, colors.red.100), …}
+        theme: Object.keys(colors.red).reduce(
+          (obj, num) => ({
+            ...obj,
+            [num]: `var(--theme-${num}, ${colors.red[num]})`,
+          }),
+          {}
+        ),
+      },
+      fontFamily: {
+        sans: ["Roboto", ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  safelist: [],
+  plugins: [require("tailwindcss-logical")],
+};
